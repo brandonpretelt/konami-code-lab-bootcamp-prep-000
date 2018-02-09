f455a2138fd4fb364bd687b4caf25f73b3a7a684
Brandon Pretelt <brandonpretelt93@gmail.com>
2018-02-09 07:00:09 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,8 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 let index = 0;
+let input = document.querySelector('input');
+let output = document.querySelector('#ouput');
 
 function onKeyDownHandler(e) {
   const key = parseInt(e.detail || e.which);
